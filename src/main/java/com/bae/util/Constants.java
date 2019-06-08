1e58c71e2df475ca45f6eb8d625977e03667354b
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-08 12:18:02 +0100

    Created constants class to hold constant values

---
+++ src/main/java/com/bae/util/Constants.java
@@ -0,0 +1,11 @@
+package com.bae.util;
+
+public final class Constants {
+
+	// Declare private default constructor
+	// This class will be only accessible via an imported static context
+	private Constants() {
+
+	}
+
+}
