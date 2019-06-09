789d849283200c041e66337cea1f5934c096773a
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-09 18:53:22 +0100

    Created GameModeDatabaseRepository (GMDBR) and functionality for retrieving all gamemodes (getAllGameMods)

---
+++ src/main/java/com/bae/persistence/repository/GameModeDatabaseRepository.java
@@ -0,0 +1,58 @@
+package com.bae.persistence.repository;
+
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import com.bae.persistence.domain.GameMode;
+import com.bae.util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
+public class GameModeDatabaseRepository implements GameModeRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager entityManager;
+
+	@Inject
+	private JSONUtil util;
+
+	@Override
+	public String getAllGameModes() {
+		TypedQuery<GameMode> query = entityManager.createQuery("SELECT g FROM GameMode g", GameMode.class);
+		Collection<GameMode> gameModes = query.getResultList();
+		return util.getJSONForObject(gameModes);
+	}
+
+	@Override
+	public String createGameMode(String gameMode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteGameMode(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateGameMode(int id, String gameMode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findGameMode(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
