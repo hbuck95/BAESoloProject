c7e564ce7f51180eea787facc57c66a4589f18e3
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-09 18:46:18 +0100

    Create championdatabaserepository and added required json responses in Constants

---
+++ src/main/java/com/bae/persistence/repository/ChampionDatabaseRepository.java
@@ -0,0 +1,86 @@
+package com.bae.persistence.repository;
+
+import static com.bae.util.Constants.CHAMPION_NOT_FOUND;
+import static com.bae.util.Constants.CREATE_CHAMPION_SUCCESS;
+import static com.bae.util.Constants.DELETE_CHAMPION_SUCCESS;
+import static com.bae.util.Constants.UPDATE_CHAMPION_SUCCESS;
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import com.bae.persistence.domain.Champion;
+import com.bae.util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
+public class ChampionDatabaseRepository implements ChampionRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager entityManager;
+
+	@Inject
+	private JSONUtil util;
+
+	@Override
+	public String getAllChampions() {
+		TypedQuery<Champion> query = entityManager.createQuery("SELECT a FROM Account a", Champion.class);
+		Collection<Champion> accounts = query.getResultList();
+		return util.getJSONForObject(accounts);
+	}
+
+	@Override
+	@Transactional(REQUIRED)
+	public String createChampion(String champion) {
+		Champion champ = util.getObjectForJSON(champion, Champion.class);
+		entityManager.persist(champ);
+		return CREATE_CHAMPION_SUCCESS;
+	}
+
+	@Override
+	@Transactional(REQUIRED)
+	public String deleteChampion(int id) {
+		Champion champ = entityManager.find(Champion.class, id);
+		entityManager.remove(champ);
+		return DELETE_CHAMPION_SUCCESS;
+	}
+
+	@Override
+	@Transactional(REQUIRED)
+	public String updateChampion(int id, String champion) {
+		Champion updatedChampion = util.getObjectForJSON(champion, Champion.class);
+
+		if (!checkChampionExists(id)) {
+			return CHAMPION_NOT_FOUND;
+		}
+
+		entityManager.merge(updatedChampion);
+		return UPDATE_CHAMPION_SUCCESS;
+	}
+
+	@Override
+	public String findChampion(int id) {
+		if (!checkChampionExists(id)) {
+			return CHAMPION_NOT_FOUND;
+		}
+
+		return util.getJSONForObject((Champion) entityManager.find(Champion.class, id));
+	}
+
+	private boolean checkChampionExists(int id) {
+		// Execute a query rather than using entityManager.find/.contains to improve
+		// performance by not having to retrieve records from the database.
+		long results = (long) entityManager.createQuery("SELECT COUNT(c) FROM Champion c WHERE c.champion_id = id")
+				.getSingleResult();
+
+		return results == 1;
+	}
+
+}
--- src/main/java/com/bae/util/Constants.java
+++ src/main/java/com/bae/util/Constants.java
@@ -8,4 +8,9 @@ public final class Constants {
 
 	}
 
+	public static final String CREATE_CHAMPION_SUCCESS = "{\"message\": \"Champion successfully created!\"}";
+	public static final String DELETE_CHAMPION_SUCCESS = "{\"message\": \"The specified champion has successfully been deleted\"}";
+	public static final String UPDATE_CHAMPION_SUCCESS = "{\"message\": \"The specified champion has successfuly been updated\"}";
+	public static final String CHAMPION_NOT_FOUND = "{\"message\": \"A champion with the specified ID could not be found\"}";
+
 }
