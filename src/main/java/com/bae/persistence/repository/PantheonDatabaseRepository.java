8a0216c58ed576eb667643da3f9b645319e4662b
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-12 12:55:00 +0100

    Added JTA Annotations

--- src/main/java/com/bae/persistence/repository/PantheonDatabaseRepository.java
+++ src/main/java/com/bae/persistence/repository/PantheonDatabaseRepository.java
@@ -1,6 +1,25 @@
 package com.bae.persistence.repository;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import com.bae.util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
 public class PantheonDatabaseRepository implements PantheonRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager entityManager;
+
+	@Inject
+	private JSONUtil util;
 
 	@Override
 	public String getAllPantheons() {
@@ -9,18 +28,21 @@ public class PantheonDatabaseRepository implements PantheonRepository {
 	}
 
 	@Override
+	@Transactional(REQUIRED)
 	public String createPantheon(String pantheon) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional(REQUIRED)
 	public String deletePantheon(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional(REQUIRED)
 	public String updatePantheon(int id, String pantheon) {
 		// TODO Auto-generated method stub
 		return null;
