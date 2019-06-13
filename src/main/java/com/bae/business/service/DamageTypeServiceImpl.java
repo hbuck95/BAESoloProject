53e6ca0d9cb03d1c407e6370677ac1bdcabc3139
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 16:27:36 +0100

    Added class DamageTypeServiceImpl implementing the DamageTypeService interface

---
+++ src/main/java/com/bae/business/service/DamageTypeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.bae.business.service;
+
+import javax.inject.Inject;
+
+import com.bae.persistence.repository.DamageTypeRepository;
+
+public class DamageTypeServiceImpl implements DamageTypeService {
+
+	@Inject
+	private DamageTypeRepository repo;
+
+	@Override
+	public String getAllDamageTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createDamageType(String damageType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteDamageType(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateDamageType(int id, String damageType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findDamageType(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
