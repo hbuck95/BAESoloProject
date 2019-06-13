c293a68c198d6531cfa2e8d983cc84ca95d0d09d
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 16:31:32 +0100

    Added ChampionGameModeStatsServiceImpl using the ChampionGameModeStatsService interface

---
+++ src/main/java/com/bae/business/service/ChampionGameModeStatsServiceImpl.java
@@ -0,0 +1,54 @@
+package com.bae.business.service;
+
+import javax.inject.Inject;
+
+import com.bae.persistence.repository.ChampionGameModeStatsRepository;
+
+public class ChampionGameModeStatsServiceImpl implements ChampionGameModeStatsService {
+
+	@Inject
+	private ChampionGameModeStatsRepository repo;
+
+	@Override
+	public String getAllChampionGameModeStats() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createChampionGameModeStats(String gameMode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteChampionGameModeStats(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateChampionGameModeStats(int id, String gameMode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findChampionGameModeStats(int gameModeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findChampionGameModeStats(String championName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findChampionGameModeStats(String championName, int gameModeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
