337a82a4546890b631eccc91542aa9cba693ec39
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 16:11:59 +0100

    Added ChampionServiceImpl class implementing the ChampionService Interface

---
+++ src/main/java/com/bae/business/service/ChampionServiceImpl.java
@@ -0,0 +1,42 @@
+package com.bae.business.service;
+
+import javax.inject.Inject;
+
+import com.bae.persistence.repository.ChampionRepository;
+
+public class ChampionServiceImpl implements ChampionService {
+
+	@Inject
+	private ChampionRepository repo;
+
+	@Override
+	public String getAllChampions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createChampion(String champion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteChampion(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateChampion(int id, String champion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findChampion(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
