ac8849a668682e09dc88ecd123b26be519f5d61e
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 17:11:17 +0100

    Added getAllChampions method to ChampionController

--- src/main/java/com/bae/rest/ChampionController.java
+++ src/main/java/com/bae/rest/ChampionController.java
@@ -1,7 +1,9 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.ChampionService;
 
@@ -10,6 +12,13 @@ public class ChampionController {
 
 	@Inject
 	private ChampionService service;
+
+	@Path("/getAllChampions")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllChampions() {
+		return service.getAllChampions();
+	}
 
 	public void setService(ChampionService service) {
 		this.service = service;
