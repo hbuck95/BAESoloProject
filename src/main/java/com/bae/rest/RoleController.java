7fd19d77931d3db55dce2b789fd453f9b7055ebb
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 18:38:24 +0100

    Added method to getAllRole in RoleController

--- src/main/java/com/bae/rest/RoleController.java
+++ src/main/java/com/bae/rest/RoleController.java
@@ -1,7 +1,9 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.RoleService;
 
@@ -10,6 +12,13 @@ public class RoleController {
 
 	@Inject
 	private RoleService service;
+
+	@Path("/getAllRoles")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllRoles() {
+		return service.getAllRoles();
+	}
 
 	public void setService(RoleService service) {
 		this.service = service;
