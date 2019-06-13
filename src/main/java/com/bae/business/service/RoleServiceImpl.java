2b3298c37b4adec6d6e524ce281d6976b41acf22
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 16:20:42 +0100

    Added class RoleServiceImpl using the RoleService interface

---
+++ src/main/java/com/bae/business/service/RoleServiceImpl.java
@@ -0,0 +1,42 @@
+package com.bae.business.service;
+
+import javax.inject.Inject;
+
+import com.bae.persistence.repository.RoleRepository;
+
+public class RoleServiceImpl implements RoleService {
+
+	@Inject
+	private RoleRepository repo;
+
+	@Override
+	public String getAllRoles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createRole(String role) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteRole(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateRole(int id, String role) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findRole(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
