1a962227feb9689ef21571acbc7013936eafb8c0
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-13 16:24:33 +0100

    Added class GameModeServiceImpl implementing the GameModeService interface

---
+++ src/main/java/com/bae/business/service/GameModeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.bae.business.service;
+
+import javax.inject.Inject;
+
+import com.bae.persistence.repository.GameModeRepository;
+
+public class GameModeServiceImpl implements GameModeService {
+
+	@Inject
+	private GameModeRepository repo;
+
+	@Override
+	public String getAllGameModes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createGameMode(String gameMode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteGameMode(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateGameMode(int id, String gameMode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findGameMode(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
