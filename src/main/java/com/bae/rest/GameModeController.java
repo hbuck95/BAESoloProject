71e25171d897d457209d4717ab21e567a6e058e2
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 11:53:56 +0100

    Added method to add a new GameMode in GameModeController

--- src/main/java/com/bae/rest/GameModeController.java
+++ src/main/java/com/bae/rest/GameModeController.java
@@ -2,6 +2,7 @@ package com.bae.rest;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -26,6 +27,13 @@ public class GameModeController {
 	@Produces({ "application/json" })
 	public String getGameMode(@PathParam("gameModeId") int id) {
 		return service.findGameMode(id);
+	}
+
+	@Path("/createGameMode")
+	@POST
+	@Produces({ "application/json" })
+	public String createGameMode(String gameMode) {
+		return service.createGameMode(gameMode);
 	}
 
 	public void setService(GameModeService service) {
