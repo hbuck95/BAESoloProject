6c88ebc671738318908e964c5f15875868788577
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 16:15:43 +0100

    Added method to get stats by game mode id in CGMSC

--- src/main/java/com/bae/rest/ChampionGameModeStatsController.java
+++ src/main/java/com/bae/rest/ChampionGameModeStatsController.java
@@ -3,6 +3,7 @@ package com.bae.rest;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.bae.business.service.ChampionGameModeStatsService;
@@ -18,6 +19,14 @@ public class ChampionGameModeStatsController {
 	@Produces({ "application/json" })
 	public String getAllChampionGameModeStats() {
 		return service.getAllChampionGameModeStats();
+	}
+
+	@Path("/getStats/{gameModeId}")
+	@GET
+	@Produces({ "application/json" })
+	public String findChampionGameModeStats(@PathParam("gameModeId") int gameModeId) {
+		return service.findChampionGameModeStats(gameModeId);
+
 	}
 
 	public void setService(ChampionGameModeStatsService service) {
