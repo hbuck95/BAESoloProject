0f518d64d357852183c7355924064627cf6bd575
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 16:00:47 +0100

    Added method to find a DamageType by id in DamageTypeController

--- src/main/java/com/bae/rest/DamageTypeController.java
+++ src/main/java/com/bae/rest/DamageTypeController.java
@@ -3,6 +3,7 @@ package com.bae.rest;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.bae.business.service.DamageTypeService;
@@ -18,6 +19,13 @@ public class DamageTypeController {
 	@Produces({ "application/json" })
 	public String getAllChampions() {
 		return service.getAllDamageTypes();
+	}
+
+	@Path("/getDamageType/{damageType}")
+	@GET
+	@Produces({ "application/json" })
+	public String findDamageType(@PathParam("damageType") int id) {
+		return service.findDamageType(id);
 	}
 
 	public void setService(DamageTypeService service) {
