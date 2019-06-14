7fc8b54ddac5d4ea404d845447990fc8743d62ef
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 11:32:49 +0100

    Added method to get allPantheons in PantheonController

--- src/main/java/com/bae/rest/PantheonController.java
+++ src/main/java/com/bae/rest/PantheonController.java
@@ -1,7 +1,9 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.PantheonService;
 
@@ -10,6 +12,13 @@ public class PantheonController {
 
 	@Inject
 	private PantheonService service;
+
+	@Path("/getAllPantheons")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllRoles() {
+		return service.getAllPantheons();
+	}
 
 	public void setService(PantheonService service) {
 		this.service = service;
