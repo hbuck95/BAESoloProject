ce700af63a6afaa7ade379e6fd6a4a7905db5767
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 16:13:37 +0100

    Added method to get all stats in CGMSC

--- src/main/java/com/bae/rest/ChampionGameModeStatsController.java
+++ src/main/java/com/bae/rest/ChampionGameModeStatsController.java
@@ -1,7 +1,9 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.ChampionGameModeStatsService;
 
@@ -10,6 +12,13 @@ public class ChampionGameModeStatsController {
 
 	@Inject
 	private ChampionGameModeStatsService service;
+
+	@Path("/getAllStats")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllChampionGameModeStats() {
+		return service.getAllChampionGameModeStats();
+	}
 
 	public void setService(ChampionGameModeStatsService service) {
 		this.service = service;
