e9de1d7966fbd09df319993280cdf0a739665d8d
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 11:37:33 +0100

    Added method to update a pantheon in pantheon controller

--- src/main/java/com/bae/rest/PantheonController.java
+++ src/main/java/com/bae/rest/PantheonController.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -42,6 +43,13 @@ public class PantheonController {
 	@Produces({ "application/json" })
 	public String deletePantheon(@PathParam("pantheonId") int id) {
 		return service.deletePantheon(id);
+	}
+
+	@Path("/updatePantheon/{pantheonId}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updatePantheon(@PathParam("pantheonId") int id, String pantheon) {
+		return service.updatePantheon(id, pantheon);
 	}
 
 	public void setService(PantheonService service) {
