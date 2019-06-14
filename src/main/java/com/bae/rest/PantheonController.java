d79df387597830e2bb041503bbc4b107939f4215
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 11:35:16 +0100

    Added method to delete a pantheon in pantheon controller

--- src/main/java/com/bae/rest/PantheonController.java
+++ src/main/java/com/bae/rest/PantheonController.java
@@ -1,9 +1,11 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.bae.business.service.PantheonService;
@@ -26,6 +28,13 @@ public class PantheonController {
 	@Produces({ "application/json" })
 	public String createPantheon(String pantheon) {
 		return service.createPantheon(pantheon);
+	}
+
+	@Path("/deletePantheon/{pantheonId}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deletePantheon(@PathParam("pantheonId") int id) {
+		return service.deletePantheon(id);
 	}
 
 	public void setService(PantheonService service) {
