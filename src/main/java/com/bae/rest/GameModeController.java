8ac253f1da494286cc69a7d79380a98b7b917139
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 11:51:43 +0100

    Added method to getAllGameModes in GameModeController

--- src/main/java/com/bae/rest/GameModeController.java
+++ src/main/java/com/bae/rest/GameModeController.java
@@ -1,7 +1,9 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.GameModeService;
 
@@ -10,6 +12,13 @@ public class GameModeController {
 
 	@Inject
 	private GameModeService service;
+
+	@Path("/getAllGameModes")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllGameModes() {
+		return service.getAllGameModes();
+	}
 
 	public void setService(GameModeService service) {
 		this.service = service;
