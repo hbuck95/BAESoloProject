315ebd2cd43f1e65d864c5d68d792bf8befba41e
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 11:54:47 +0100

    Added method to delete a GameMode in GameModeController

--- src/main/java/com/bae/rest/GameModeController.java
+++ src/main/java/com/bae/rest/GameModeController.java
@@ -1,6 +1,7 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -34,6 +35,13 @@ public class GameModeController {
 	@Produces({ "application/json" })
 	public String createGameMode(String gameMode) {
 		return service.createGameMode(gameMode);
+	}
+
+	@Path("/deleteGameMode/{gameModeId}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deleteGameMode(@PathParam("gameModeId") int id) {
+		return service.deleteGameMode(id);
 	}
 
 	public void setService(GameModeService service) {
