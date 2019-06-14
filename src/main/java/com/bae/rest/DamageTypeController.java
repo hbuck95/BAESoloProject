a7fe5acf25cbdd71b99cd3a8d61717f7d2b3b1b9
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-14 15:59:28 +0100

    Added method to get alldamagetypes in damagetype controller

--- src/main/java/com/bae/rest/DamageTypeController.java
+++ src/main/java/com/bae/rest/DamageTypeController.java
@@ -1,7 +1,9 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.DamageTypeService;
 
@@ -10,6 +12,13 @@ public class DamageTypeController {
 
 	@Inject
 	private DamageTypeService service;
+
+	@Path("/getAllDamageTypes")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllChampions() {
+		return service.getAllDamageTypes();
+	}
 
 	public void setService(DamageTypeService service) {
 		this.service = service;
