d3a6b31cbd4a800f434a8af5542556d8c406b276
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-17 20:03:30 +0100

    Added mockito test for deleteRole in RDBR

--- src/test/java/com/bae/tests/persistence/repository/RoleDatabaseRepositoryTest.java
+++ src/test/java/com/bae/tests/persistence/repository/RoleDatabaseRepositoryTest.java
@@ -3,7 +3,8 @@ package com.bae.tests.persistence.repository;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -33,7 +34,7 @@ public class RoleDatabaseRepositoryTest {
 	private Query query;
 
 	private JSONUtil util;
-
+	private Map<Integer, Role> roleMap;
 	private static final String MOCK_ROLE_ARRAY = "[{\"id\":1,\"name\":\"Guardian\"}]";
 	private static final String MOCK_ROLE_OBJECT = "{\"id\":1,\"name\":\"Guardian\"}";
 
@@ -42,23 +43,37 @@ public class RoleDatabaseRepositoryTest {
 		repo.setEntityManager(entityManager);
 		util = new JSONUtil();
 		repo.setUtil(util);
+		roleMap = new HashMap<>();
+		roleMap.put(1, new Role.Builder().id(1).name("Guardian").build());
 	}
 
 	@Test
 	public void testGetAllRoles() {
 		Mockito.when(entityManager.createQuery(Mockito.anyString())).thenReturn(query);
-		List<Role> roles = new ArrayList<>();
-		roles.add(new Role.Builder().id(1).name("Guardian").build());
-		Mockito.when(query.getResultList()).thenReturn(roles);
+		Mockito.when(query.getResultList()).thenReturn(new ArrayList<Role>(roleMap.values()));
 		assertEquals(MOCK_ROLE_ARRAY, repo.getAllRoles());
 	}
 
 	@Test
 	public void testFindRole() {
-		List<Role> roles = new ArrayList<>();
-		roles.add(new Role.Builder().id(1).name("Guardian").build());
-		Mockito.when(entityManager.find(Role.class, 1)).thenReturn(roles.get(0));
+		Mockito.when(entityManager.find(Role.class, 1)).thenReturn(roleMap.get(1));
 		assertEquals(MOCK_ROLE_OBJECT, repo.findRole(1));
+	}
+
+	@Test
+	public void testDeleteRole() {
+		Role roleA = roleMap.get(1);
+
+		Mockito.when(entityManager.find(Role.class, 1)).thenReturn(roleA);
+		entityManager.remove(roleA);
+		Mockito.verify(entityManager, Mockito.times(1)).remove(roleA);
+
+		// Mockito.when(repo.deleteRole(1)).thenReturn(Constants.DELETE_ROLE_SUCCESS);
+
+		// Mockito.verify(entityManager,
+		// Mockito.times(5)).remove(Constants.DELETE_ROLE_SUCCESS);
+		// assertEquals(Constants.DELETE_ROLE_SUCCESS, res);
+
 	}
 
 }
