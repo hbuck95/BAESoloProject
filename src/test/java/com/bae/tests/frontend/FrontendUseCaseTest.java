6dc2aac897e44d3311b7cd47c79bd252928112ea
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-19 15:49:05 +0100

    Added Selenium test for use case 6

--- src/test/java/com/bae/tests/frontend/FrontendUseCaseTest.java
+++ src/test/java/com/bae/tests/frontend/FrontendUseCaseTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -75,6 +77,90 @@ public class FrontendUseCaseTest {
 		driver.findElement(By.xpath("//*[@id=\"navbarNavAltMarkup\"]/div/a[3]")).click();
 		assertEquals(ROOT_LOCATION + "stats.html", driver.getCurrentUrl());
 		body();
+	}
+
+	@Test
+	public void useCase6() { // Add a new champions details
+		driver.findElement(By.xpath("//*[@id=\"navbarNavAltMarkup\"]/div/a[2]")).click();
+		assertEquals(ROOT_LOCATION + "champions.html", driver.getCurrentUrl());
+
+		WebElement ele;
+		String name = "Fred";
+		String pantheon = "Roman";
+		String role = "Mage";
+		String damageType = "Physical";
+		String hp = "380";
+		String damage = "32";
+
+		// Wait until the page has loaded
+		ele = (new WebDriverWait(driver, 15)).until(ExpectedConditions.presenceOfElementLocated(By.id("new-btn")));
+		ele.click();
+
+		// Wait until the modal has loaded
+		ele = (new WebDriverWait(driver, 15))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.id("new-submit-btn")));
+
+		// Send the values for the new champion
+		ele = driver.findElement(By.id("new-name"));
+		ele.sendKeys(name);
+		assertEquals(name, ele.getAttribute("value"));
+
+		ele = driver.findElement(By.id("new-role-selection"));
+		ele.click();
+		ele.sendKeys(Keys.ARROW_DOWN);
+		ele.sendKeys(Keys.ARROW_DOWN);
+		ele.sendKeys(Keys.ENTER);
+		assertEquals(role, ele.getAttribute("value"));
+
+		ele = driver.findElement(By.id("new-pantheon-selection"));
+		ele.click();
+		ele.sendKeys(Keys.ARROW_DOWN);
+		ele.sendKeys(Keys.ARROW_DOWN);
+		ele.sendKeys(Keys.ENTER);
+		assertEquals(pantheon, ele.getAttribute("value"));
+
+		ele = driver.findElement(By.id("new-damage-selection"));
+		ele.click();
+		ele.sendKeys(Keys.ARROW_DOWN);
+		ele.sendKeys(Keys.ENTER);
+		assertEquals(damageType, ele.getAttribute("value"));
+
+		ele = driver.findElement(By.id("new-hp"));
+		ele.sendKeys(hp);
+		assertEquals(hp, ele.getAttribute("value"));
+
+		ele = driver.findElement(By.id("new-damage"));
+		ele.click();
+		ele.sendKeys(damage);
+		assertEquals(damage, ele.getAttribute("value"));
+
+		driver.findElement(By.id("new-submit-btn")).click();
+
+		new WebDriverWait(driver, 60).ignoring(NoAlertPresentException.class)
+				.until(ExpectedConditions.alertIsPresent());
+
+		assertEquals("Champion successfully created!", driver.switchTo().alert().getText());
+
+		driver.switchTo().alert().accept();
+
+		// Wait for the element to load
+		ele = (new WebDriverWait(driver, 15)).until(ExpectedConditions.visibilityOfElementLocated(By.id("104")));
+
+		// Check that it was created correctly
+		ele = driver.findElement(By.xpath("//*[@id=\"tbl\"]/tbody/tr[104]/td[2]"));
+		assertEquals(name, ele.getText());
+		ele = driver.findElement(By.xpath("//*[@id=\"tbl\"]/tbody/tr[104]/td[3]"));
+		assertEquals(role, ele.getText());
+		ele = driver.findElement(By.xpath("//*[@id=\"tbl\"]/tbody/tr[104]/td[4]"));
+		assertEquals(pantheon, ele.getText());
+		ele = driver.findElement(By.xpath("//*[@id=\"tbl\"]/tbody/tr[104]/td[5]"));
+		assertEquals(damageType, ele.getText());
+		ele = driver.findElement(By.xpath("//*[@id=\"tbl\"]/tbody/tr[104]/td[6]"));
+		assertEquals(hp, ele.getText());
+		ele = driver.findElement(By.xpath("//*[@id=\"tbl\"]/tbody/tr[104]/td[7]"));
+		assertEquals(damage, ele.getText());
+
+		// body();
 	}
 
 	public void body() {
